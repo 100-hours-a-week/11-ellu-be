9e522fc44311ce44c31a6c2c281512ac8299eb23
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-05-12 13:49:16 +0900

    Revert "fix: meeting note dto id field 타입 수정 (#56)"
    This reverts commit 14abcc34c3e5049500f7c600b6063e32b8a56d61.

--- src/main/java/com/ellu/looper/dto/MeetingNoteRequest.java
+++ src/main/java/com/ellu/looper/dto/MeetingNoteRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MeetingNoteRequest {
-  private int project_id;
+  private Long project_id;
   private String content;
   private String nickname;
   private String position;
