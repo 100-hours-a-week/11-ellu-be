acae843c007d01abe21f63a3764c412946617b97
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-05-26 02:09:38 +0900

    Feat/#95 kafka for notification (#96)
    * feat:초대 알림 sse
    
    * feat: 초대 알림기능에 kafka 관련 코드 구현

---
+++ src/main/java/com/ellu/looper/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.ellu.looper.repository;
+
+import com.ellu.looper.entity.Notification;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+  List<Notification> findByReceiverIdAndDeletedAtIsNullOrderByCreatedAtDesc(Long receiverId);
+}
+
+
+
