b71e510b71dc5b9e047f8e59c470bb022546ca76
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-06-09 23:15:40 +0900

    fix: 프로젝트 알림 수신자, 송신자가 잘못 지정된 버그 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO public.notification_template (type, template)
 VALUES
   ('PROJECT_INVITED', '{creator}님이 {project} 프로젝트에 회원님을 {position}포지션으로 초대했습니다.'),
-  ('PROJECT_EXPELLED', '{project} 프로젝트에서 회원님을 내보냈습니다. 해당 프로젝트와 관련된 모든 일정과 정보는 더 이상 보이지 않습니다.'),
-  ('PROJECT_DELETED', '{project} 프로젝트가 삭제되었습니다. 해당 프로젝트와 관련된 모든 일정과 정보는 더 이상 보이지 않습니다.'),
+  ('PROJECT_EXPELLED', '{project} 프로젝트에서 회원님을 내보냈습니다. 해당 프로젝트의 일정과 정보는 더 이상 보이지 않습니다.'),
+  ('PROJECT_DELETED', '{project} 프로젝트가 삭제되었습니다. 해당 프로젝트의 일정과 정보는 더 이상 보이지 않습니다.'),
   ('SCHEDULE_CREATED', '{project}프로젝트에 새로운 {schedule}일정이 추가되었습니다.'),
   ('SCHEDULE_UPDATED', '{project}프로젝트에 {schedule}일정이 업데이트되었습니다.'),
   ('SCHEDULE_DELETED', '{project}프로젝트의 {schedule} 일정이 삭제되었습니다.'),
