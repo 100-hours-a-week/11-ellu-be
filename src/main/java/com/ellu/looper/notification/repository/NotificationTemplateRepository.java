d2398d87fb944b5111429becfaff7db0fa3f6eab
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-06-09 15:15:37 +0900

    chore: 알림 템플릿 초기화하는 관리자용 api 추가

--- src/main/java/com/ellu/looper/notification/repository/NotificationTemplateRepository.java
+++ src/main/java/com/ellu/looper/notification/repository/NotificationTemplateRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface NotificationTemplateRepository extends JpaRepository<NotificationTemplate, Long> {
 
   Optional<NotificationTemplate> findByType(NotificationType notificationType);
+
+  boolean existsByType(NotificationType key);
 }
