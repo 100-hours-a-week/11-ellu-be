08fa6c94eff603a49d67e2307b283a5424bfdfe4
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-06-12 14:53:26 +0900

    chore: branch 변경 및 불필요한 파일 제거

--- src/main/java/com/ellu/looper/kafka/config/KafkaStreamsConfig.java
+++ src/main/java/com/ellu/looper/kafka/config/KafkaStreamsConfig.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
@@ -20,11 +21,14 @@ import org.springframework.kafka.support.serializer.JsonSerde;
 @EnableKafkaStreams
 public class KafkaStreamsConfig {
 
+  @Value("${spring.kafka.bootstrap-servers}")
+  private String bootstrapServers;
+
   @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
   public KafkaStreamsConfiguration kStreamsConfig() {
     Map<String, Object> props = new HashMap<>();
     props.put(APPLICATION_ID_CONFIG, "chat-response-streams");
-    props.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
     props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
     props.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class.getName());
     props.put(PROCESSING_GUARANTEE_CONFIG, EXACTLY_ONCE_V2);
