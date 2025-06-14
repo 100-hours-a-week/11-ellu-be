77668d985ea3b33a6b649ac4d71deabe3ab4f181
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-06-14 09:38:30 +0900

    fix: 챗봇 api 누락된 dto 필드 추가

--- src/main/java/com/ellu/looper/chat/dto/MessageRequest.java
+++ src/main/java/com/ellu/looper/chat/dto/MessageRequest.java
@@ -1,6 +1,9 @@
 package com.ellu.looper.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class MessageRequest {
   @NotBlank private String message;
+  @NotNull private String user_id;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  private LocalDateTime date;
 }
