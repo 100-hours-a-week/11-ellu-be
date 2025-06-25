20dcf7b48026ef487ae756c45328278bf8b23c40
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-06-25 22:50:07 +0900

    refactor: refresh_token 인덱스 추가

--- src/main/java/com/ellu/looper/auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/ellu/looper/auth/repository/RefreshTokenRepository.java
@@ -1,18 +1,18 @@
 package com.ellu.looper.auth.repository;
 
 import com.ellu.looper.auth.entity.RefreshToken;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
-  // Custom query to find a RefreshToken by its associated member ID
-  Optional<RefreshToken> findByUserId(Long userId);
 
-  // Optional custom query to delete a RefreshToken by member ID
-  void deleteByUserId(Long userId);
+  @Query("SELECT rt FROM RefreshToken rt JOIN FETCH rt.user WHERE rt.user.id = :userId")
+  Optional<RefreshToken> findByUserId(@Param("userId") Long userId);
 
   Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
