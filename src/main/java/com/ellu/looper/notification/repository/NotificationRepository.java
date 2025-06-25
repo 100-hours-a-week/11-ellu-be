cfd170dcb21f4a5567bf506a140a51b780858f8a
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-06-26 00:34:11 +0900

    refactor: notification N+1문제 해결

--- src/main/java/com/ellu/looper/notification/repository/NotificationRepository.java
+++ src/main/java/com/ellu/looper/notification/repository/NotificationRepository.java
@@ -4,11 +4,15 @@ import com.ellu.looper.notification.entity.Notification;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+  @EntityGraph(attributePaths = {"sender", "template"})
   List<Notification> findByReceiverIdAndDeletedAtIsNullOrderByCreatedAtDesc(Long receiverId);
 
+  @EntityGraph(attributePaths = {"sender", "template"})
   Optional<Notification> findByIdAndDeletedAtIsNull(Long notificationId);
 
   List<Notification> findByCreatedAtBeforeAndDeletedAtIsNull(LocalDateTime oneWeekAgo);
