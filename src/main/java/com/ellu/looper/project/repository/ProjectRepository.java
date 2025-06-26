ae903af1c4610e74f8fc096d997c601ae72a964c
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-06-26 09:46:13 +0900

    chore: 사용되지 않는 불필요한 코드 제거

--- src/main/java/com/ellu/looper/project/repository/ProjectRepository.java
+++ src/main/java/com/ellu/looper/project/repository/ProjectRepository.java
@@ -1,17 +1,10 @@
 package com.ellu.looper.project.repository;
 
 import com.ellu.looper.project.entity.Project;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
   Optional<Project> findByIdAndDeletedAtIsNull(Long projectId);
-
-  @Query(
-      "SELECT pm.project FROM ProjectMember pm WHERE pm.user.id = :memberId AND pm.project.deletedAt IS NULL")
-  List<Project> findAllByMemberId(@Param("memberId") Long memberId);
 }
