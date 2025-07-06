81018da1a0804595d894e39e188e775c94601f46
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-07-06 21:44:15 +0900

    fix: fastapi api 수정에 따른 수정 및 코드 리팩토링

--- src/main/java/com/ellu/looper/fastapi/dto/TaskSelectionResultRequest.java
+++ src/main/java/com/ellu/looper/fastapi/dto/TaskSelectionResultRequest.java
@@ -2,4 +2,4 @@ package com.ellu.looper.fastapi.dto;
 
 import java.util.List;
 
-public record TaskSelectionResultRequest(Long project_id, List<SchedulePreview> content) {}
+public record TaskSelectionResultRequest(List<SchedulePreview> content) {}
